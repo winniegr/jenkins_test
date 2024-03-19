da81f50822a83be533627f8b680680870cfe8e05
winniegr <gerong1008@gmail.com>
2024-03-19 13:07:23 +0800

    Update build.gradle.kts

--- communicationservice-android/build.gradle.kts
+++ communicationservice-android/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    `maven-publish`
 }
 
 android {
@@ -31,55 +30,6 @@ android {
         unitTests {
             isIncludeAndroidResources = true
             isReturnDefaultValues = true
-        }
-    }
-}
-
-dependencies {
-    api(libs.bsci.communication.service)
-    implementation(libs.ably.android)
-    implementation(libs.retrofit.core)
-    implementation(libs.retrofit.converter.gson)
-    implementation(libs.retrofit.converter.scalars)
-    implementation(libs.okhttp.logging)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    testImplementation(libs.mockk)
-    testImplementation(libs.kotlin.test)
-    testImplementation(libs.coroutines.test)
-    androidTestImplementation(libs.androidx.test.ext.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
-
-tasks {
-    val androidSourcesJar by creating(Jar::class) {
-        archiveClassifier.set("sources")
-        from(android.sourceSets["main"].java.srcDirs)
-    }
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = "com.bsci.communication"
-                artifactId = "communication-android"
-                version = "0.0.1"
-
-                artifact(tasks["androidSourcesJar"])
-            }
-        }
-        repositories {
-            maven {
-                url = uri("s3://bscmavenrepo/releases")
-                credentials(AwsCredentials::class) {
-                    accessKey = findProperty("AWS_MAVEN_S3_ACCESS_KEY") as String
-                    secretKey = findProperty("AWS_MAVEN_S3_SECRET_KEY") as String
-                }
-            }
         }
     }
 }
